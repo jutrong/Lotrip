26e6b1c4a65207f76f3bb380d41d4bd0aa68b84b
Jutrong <leek71358@gmail.com>
2024-03-06 11:35:58 +0900

    Feat: 호텔,룸 mock 데이터 생성

--- src/components/test/HotelListAddButton.tsx
+++ src/components/test/HotelListAddButton.tsx
@@ -1,6 +1,9 @@
 import Button from '@shared/Button'
 
-import { EVENTS, HOTEL, HOTEL_NAMES, IMAGES } from '@/mock/data'
+import { EVENTS, HOTEL, HOTEL_NAMES, IMAGES, ROOMS } from '@/mock/data'
+import { COLLECTIONS } from '@/constants'
+import { store } from '@/remote/firebase'
+import { collection, doc, writeBatch } from 'firebase/firestore'
 
 // min에서 max 범위의 랜덤값 가져오기
 const random = (min: number, max: number) => {
@@ -8,6 +11,8 @@ const random = (min: number, max: number) => {
 }
 
 const HotelListAddButton = () => {
+  const batch = writeBatch(store)
+
   const handleButtonClick = () => {
     const hotels = HOTEL_NAMES.map((hotelName, idx) => {
       return {
@@ -20,8 +25,18 @@ const HotelListAddButton = () => {
         ...(EVENTS[idx] != null && { events: EVENTS[idx] }),
       }
     })
+    // COLLECTIONS.HOTEL안에 COLLETIONS.ROOM이 있는 계층구조로 데이터를 추가
+    hotels.forEach((hotel) => {
+      const hotelDocRef = doc(collection(store, COLLECTIONS.HOTEL))
 
-    console.log(hotels)
+      batch.set(hotelDocRef, hotel)
+
+      ROOMS.forEach((room) => {
+        const subDocRef = doc(collection(hotelDocRef, COLLECTIONS.ROOM))
+        batch.set(subDocRef, room)
+      })
+    })
+    batch.commit()
   }
 
   return <Button onClick={handleButtonClick}>호텔 리스트 추가</Button>
