eb2e43e2e58d666ea0d3a21ea503b362fb71e35e
Jutrong <leek71358@gmail.com>
2024-03-13 20:16:56 +0900

    Feat:  상세게시물 마크다운 contens

--- src/pages/Hotel.tsx
+++ src/pages/Hotel.tsx
@@ -2,7 +2,8 @@ import { useParams } from 'react-router-dom'
 
 import useHotel from '@components/hotel/hooks/useHotel'
 import Top from '@components/shared/Top'
-import Carousel from '@/components/hotel/Carousel'
+import Carousel from '@components/hotel/Carousel'
+import Contents from '@components/hotel/Contents'
 
 const Hotel = () => {
   const { id } = useParams() as { id: string }
@@ -11,12 +12,13 @@ const Hotel = () => {
 
   if (data == null || isLoading) return <div>Loading...</div>
 
-  const { name, comment, images } = data
+  const { name, comment, images, contents } = data
 
   return (
     <div>
       <Top title={name} subTitle={comment} />
       <Carousel images={images} />
+      <Contents contents={contents} />
     </div>
   )
 }
